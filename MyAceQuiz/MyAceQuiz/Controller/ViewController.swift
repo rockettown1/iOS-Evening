07d9baad7d5f93e8b5288d2744976ba95f0f274c
Dan Krishnan <dan@wearecodenation.com>
2019-05-07 03:12:56 +0100

    Files for iOS evening

---
+++ MyAceQuiz/MyAceQuiz/Controller/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  MyAceQuiz
+//
+//  Created by Dan on 05/05/2019.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    
+    func updateUI() {
+        
+    }
+    
+    
+    func nextQuestion() {
+        
+    }
+    
+    
+    func checkAnswer() {
+        
+    }
+    
+    
+    func startOver() {
+        
+    }
+    
+    
+
+}
+
---
+++ TheInfamousDiceGame/TheInfamousDiceGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheInfamousDiceGame
+//
+//  Created by Dan on 05/05/2019.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
